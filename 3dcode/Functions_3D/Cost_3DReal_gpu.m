944a746e5f462508e1bcdde2f9d3a558e80fa15d
Wenxiu Wang <59704238+wexw@users.noreply.github.com>
2023-05-30 19:17:16 +0200

    Add files via upload

---
+++ 3dcode/Functions_3D/Cost_3DReal_gpu.m
@@ -0,0 +1,25 @@
+function [cost,cost_norm]=Cost_3DReal_gpu(V,TV,R,t,particles,CCD_pixels)
+    if nargin <6
+       CCD_pixels = 130;
+    end
+K_c= size(TV,1); 
+
+cost = zeros(K_c,K_c); 
+cost_norm = zeros(K_c,K_c); 
+SigmaSquare1=cellfun(@(particles)  [particles.sigma(:,1).^2.*CCD_pixels^2 particles.sigma(:,2).^2.*CCD_pixels^2],particles,'uniformoutput',false);
+for i = 1:K_c
+    for j = (i+1):K_c
+            S.points = V{i}';           %take S in native orientation
+            M.points = V{j}'; 
+            
+            Mfinal = TV{j};             %final orientation of M
+            Mback = R{i}'*(Mfinal-t{i});%inverse transform M with the transformation of S
+            [~,~,procr] = procrustes(Mback',V{j}','scaling',false,'reflection',false);   %find transformation how to transform M(native) to inverse transformed M
+            M.points = M.points*procr.T + procr.c;  %do transformation
+            S.sigma =SigmaSquare1{1,i};
+            M.sigma =SigmaSquare1{1,j};
+            
+            cost(i,j) = mex_expdist(S.points, M.points, correct_uncer(S.sigma), M.sigma, procr.T);    %for now: rot-matrix set to identity matrix (IS WRONG)
+            cost_norm(i,j) = cost(i,j)/(size(M.points,1)*size(S.points,1));
+    end
+end
--- 3dcode/Functions_3D/Run_DissimilarityMatrixCalculation3D.m
+++ 3dcode/Functions_3D/Run_DissimilarityMatrixCalculation3D.m
@@ -3,12 +3,26 @@
    tic
 %V2's unit is nm, V2 is the original input particles' localizations
    cost_norm_All=cell(Repeattimes,1);
-   for J_C1=1:Repeattimes%%%%%%%%%%%%%%
+   if USE_GPU_EXPDIST ==1
+       disp('Start to calculate Dissimilarity Matrix by GPU');
+   parfor J_C1=1:Repeattimes%%%%%%%%%%%%%%
        TV_p=TV_All{ J_C1,1};
        R_p=R_All{ J_C1,1};
        t_p=t_All{ J_C1,1};
-       [~, MatrixAfterAll2all_norm]=Cost_3DReal(V2,TV_p,R_p,t_p,Particles1,Cscale);
+       [~, MatrixAfterAll2all_norm]=Cost_3DReal_gpu(V2,TV_p,R_p,t_p,Particles1,Cscale);
        cost_norm_All{ J_C1,1}=MatrixAfterAll2all_norm;
+   end
+disp(' Dissimilarity Matrix Calculation finished by GPU');
+   else 
+       disp('Start to calculate Dissimilarity Matrix by CPU');
+       parfor J_C1=1:Repeattimes%%%%%%%%%%%%%%
+       TV_p=TV_All{ J_C1,1};
+       R_p=R_All{ J_C1,1};
+       t_p=t_All{ J_C1,1};
+       [~, MatrixAfterAll2all_norm]=Cost_3DReal_cpu(V2,TV_p,R_p,t_p,Particles1,Cscale);
+       cost_norm_All{ J_C1,1}=MatrixAfterAll2all_norm;
+       end
+disp(' Dissimilarity Matrix Calculation finished by CPU');
    end
    %----------------Calculate Cost Function-----------
 
