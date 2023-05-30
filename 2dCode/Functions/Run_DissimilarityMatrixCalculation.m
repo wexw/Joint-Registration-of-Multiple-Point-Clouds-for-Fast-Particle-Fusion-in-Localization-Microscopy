53eeb947feff4cbc887a8dc0908776d790f1f5ee
Wenxiu Wang <59704238+wexw@users.noreply.github.com>
2023-05-30 15:59:11 +0200

    Add files via upload

--- 2dCode/Functions/Run_DissimilarityMatrixCalculation.m
+++ 2dCode/Functions/Run_DissimilarityMatrixCalculation.m
@@ -5,10 +5,29 @@ clus_All=cell(Repeattimes,1);
 ClusterView1_All=cell(Repeattimes,1);
 cost_norm_All=cell(Repeattimes,1);
 clusfull_All=cell(Repeattimes,1);
+
+if USE_GPU_EXPDIST==1
+    disp('Start to calculate Dissimilarity Matrix by GPU');
+
 parfor J_C1=1:Repeattimes%%%%%%%%%%%%%%
     TV_p=TV_All{ J_C1,1};      %Input transport particles from the JRMPC
     % Calculate the cost matrix
-    [~,  cost_norm_All{ J_C1,1}]=Cost_2D(TV_p,Particles1,CCD_pixelsize); 
+    tic
+    [~,  cost_norm_All{ J_C1,1}]=Cost_2D_gpu(TV_p,Particles1,CCD_pixelsize); 
+toc
 end
 time_Dissimilarity=toc;
-disp(' Dissimilarity Matrix Calculation finished');
+disp(' Dissimilarity Matrix Calculation finished by GPU');
+else
+      disp('Start to calculate Dissimilarity Matrix by CPU');
+parfor J_C1=1:Repeattimes%%%%%%%%%%%%%%
+    TV_p=TV_All{ J_C1,1};      %Input transport particles from the JRMPC
+    % Calculate the cost matrix
+    tic
+    [~,  cost_norm_All{ J_C1,1}]=Cost_2D_cpu(TV_p,Particles1,CCD_pixelsize); 
+toc
+end
+time_Dissimilarity=toc;
+disp(' Dissimilarity Matrix Calculation finished by CPU');
+
+end
