e1b864df72e457d3da40c82b8850572482f02e9f
Wenxiu Wang <59704238+wexw@users.noreply.github.com>
2023-06-12 17:32:00 +0200

    Add files via upload

--- 3d/Functions_3D/Run_DissimilarityMatrixCalculation3D.m
+++ 3d/Functions_3D/Run_DissimilarityMatrixCalculation3D.m
@@ -15,18 +15,18 @@ if USE_GPU_EXPDIST ==1
     disp(' Dissimilarity Matrix Calculation finished by GPU');
 else
     disp('Start to calculate Dissimilarity Matrix by CPU');
-
+    
     if isempty(gcp('nocreate'))
-        parpool('local',40);
+        parpool('local',20);
     end
     for J_C1=1:Repeattimes%%%%%%%%%%%%%%
         TV_p=TV_All{ J_C1,1};
         R_p=R_All{ J_C1,1};
         t_p=t_All{ J_C1,1};
-
-
+        
+        
         [~, MatrixAfterAll2all_norm]=Cost_3DReal_cpu(V2,TV_p,R_p,t_p,Particles1,Cscale);
-
+        
         cost_norm_All{J_C1,1}=MatrixAfterAll2all_norm;
     end
     disp(' Dissimilarity Matrix Calculation finished by CPU');
@@ -38,4 +38,3 @@ end
 %----------------Calculate Cost Function-----------
 
 time_Dissimilarity=toc;
- 
--- 3d/Functions_3D/Run_JRMPC3D.m
+++ 3d/Functions_3D/Run_JRMPC3D.m
@@ -31,51 +31,14 @@ for JK=1:Repeattimes
     TV_All{JK,1}=cellfun(@(TV) TV.*Cscale,TV ,'uniformoutput',false);                    %all transformed particles from PMKth JRMPC
 %...-----------------Calculate Cost Function-----------
 
-tic
-%V2's unit is nm, V2 is the original input particles' localizations
-cost_norm_All=cell(Repeattimes,1);
-if USE_GPU_EXPDIST ==1
-    disp('Start to calculate Dissimilarity Matrix by GPU');
-    for J_C1=1:Repeattimes%%%%%%%%%%%%%%
-        TV_p=TV_All{ J_C1,1};
-        R_p=R_All{ J_C1,1};
-        t_p=t_All{ J_C1,1};
-        [~, MatrixAfterAll2all_norm]=Cost_3DReal_gpu(V2,TV_p,R_p,t_p,Particles1,Cscale);
-        cost_norm_All{J_C1,1}=MatrixAfterAll2all_norm;
-    end
-    disp(' Dissimilarity Matrix Calculation finished by GPU');
-else
-    disp('Start to calculate Dissimilarity Matrix by CPU');
 
-    if isempty(gcp('nocreate'))
-        parpool('local',40);
-    end
-    for J_C1=1:Repeattimes%%%%%%%%%%%%%%
-        TV_p=TV_All{ J_C1,1};
-        R_p=R_All{ J_C1,1};
-        t_p=t_All{ J_C1,1};
-
-
-        [~, MatrixAfterAll2all_norm]=Cost_3DReal_cpu(V2,TV_p,R_p,t_p,Particles1,Cscale);
-
-        cost_norm_All{J_C1,1}=MatrixAfterAll2all_norm;
-    end
-    disp(' Dissimilarity Matrix Calculation finished by CPU');
-    poolobj = gcp('nocreate'); % If no pool, do not create new one.
-    if ~isempty(poolobj)
-        delete(poolobj);
-    end
-end
-%----------------Calculate Cost Function-----------
-
-time_Dissimilarity=toc;
     X_All{JK,1}= X.*Cscale;
     X_inAll{JK,1}=Xin1.*Cscale;
     S_All{JK,1}=S.*Cscale;
 
     %------------------JRMPC 2D-----------------
 end
-disp('JRMPC 3D parfor finished');
+disp('JRMPC 3D for finished');
 toc
 Time_JRMPC=toc;
     
