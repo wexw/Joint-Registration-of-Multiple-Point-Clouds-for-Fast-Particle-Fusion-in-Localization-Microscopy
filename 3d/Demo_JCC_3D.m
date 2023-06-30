c4d16ff9357cfa9bbd84966c313b472225fcf8a6
Wenxiu Wang <59704238+wexw@users.noreply.github.com>
2023-06-30 15:07:42 +0200

    Add files via upload

--- 3d/Demo_JCC_3D.m
+++ 3d/Demo_JCC_3D.m
@@ -1,4 +1,4 @@
-%New 3D JRMPC+Classify+Connect
+%3D JRMPC+Classify+Connect
 clc
 clear
 %close all
@@ -13,9 +13,12 @@ addpath(path_function)
 
 
 % CPU/GPU settings (CPU = 0, GPU = 1)
-USE_GPU_GAUSSTRANSFORM = 1;
-USE_GPU_EXPDIST = 1;
-
+USE_GPU_GAUSSTRANSFORM = 0;
+USE_GPU_EXPDIST =0;
+if USE_GPU_EXPDIST==0
+    disp(['Please set the number of cores used here: Core_numer=']);
+    Core_number=40;%%%You can set this number based on your hardware
+end
 cluster_mode=2;          %1: H,2:MDS
 Repeattimes=2;           %Number of independent JCC progress for a single NUP model
 sizeview=120; %radius of Field of interest
@@ -25,54 +28,30 @@ load('SimulatedNup107With65%DOL.mat')
 
 Particles=particles;
 
-
-
-%dol=0.4
-N=size(Particles,2);
-
-for i=1:N % make all the particles around (0,0,0)
-    Particles{1,i}.points(:,1:3)= Particles{1,i}.points(:,1:3)-mean(Particles{1,i}.points(:,1:3));
-    max_nm(i)=max(max(Particles{1,i}.points(:,1:3)));
-end
-Cscale=round(max(max_nm));
-for i=1:N % to make all the particles inside the unit box
-    Particles{1,i}.points(:,1:3)= Particles{1,i}.points(:,1:3)./Cscale-mean(Particles{1,i}.points(:,1:3)./Cscale);
-    Particles{1,i}.sigma(:,1:2)=   Particles{1,i}.sigma(:,1:2)./Cscale;
-end
-minLo=10;
-maxLo=10000;%maximumlocalization in one particle
-%Prepare the input for the JRMPC
-[~,V1,meanlocs,~,~] = ProcessedExperimentalDataPraticle(Particles,1,N,minLo,maxLo);
-[Particles1,V2,~,~,~] = ProcessedExperimentalDataPraticle(Particles,Cscale,N,minLo,maxLo);
-
-
-M=size(V1,1);
-raster_spacing=40;       % expected resolution [nm]
-K = 18;
-%K=K_estimate(V2,meanlocs,20,0.5*raster_spacing, CCD_pixelsize);%Estimate number of Gaussian models
-
-Sa=sqrt(1000)./Cscale;% Size of initial Gaussian components
-
+Prapare_Input_particles
 
 %Step1: Joint registration
-Run_JRMPC3D
-
-
-
-
-Run_DissimilarityMatrixCalculation3D
-
-
+%Prepare parameters for JRMCP
+K = 18;
+%K=K_estimate(V2,meanlocs,20,0.5*raster_spacing, CCD_pixelsize);%Estimate number of Gaussian models
+Sa=sqrt(1000)./Cscale;% Size of initial Gaussian components
 nc=2; %number of clusters
 cluster_mode=2; %2: MDS clustering, 1: Hierarchical agglomerative clustering approach as alternative to MDS
 minClustSize=M/(nc+1);  %The minumum number of  the particle
-%Step3: Classification
-
 dimention=30;%round(N/6)*10; If classification doesn't work, try different dimention values
+
+Run_JRMPC3D
+
+
+%Step2A: Classification:Dissimilarity Matrix Calculation
+Run_DissimilarityMatrixCalculation3D
+
+
+%Step2B: Classification
 Run_Classification3D
 
 
-%Step4: Connection
+%Step3 Connection
 Run_Connection    %no need to change anything inside
 
 time_All=Time_JRMPC+time_Dissimilarity+Time_Connect+time_Classification; % Total Computational Time
@@ -81,7 +60,9 @@ time_All=Time_JRMPC+time_Dissimilarity+Time_Connect+time_Classification; % Total
 % data2c=cell2mat(TVPick')';
 
 %filenameTVpick2=[ 'UncorrectedNPCK=' num2str(K)    'S=' num2str(Sa) 'nc=' num2str(nc) 'Size=' num2str(effectiveParticlesNumber) 'dimention=' num2str(dimention) '.avi' ];
-
+%Draw the Plot by renderprojection
 Draw3DAviProjection
-% Draw3DFigure
+
+%%%You can also Draw the Plot by density plot
+%Draw3DFigure
 
