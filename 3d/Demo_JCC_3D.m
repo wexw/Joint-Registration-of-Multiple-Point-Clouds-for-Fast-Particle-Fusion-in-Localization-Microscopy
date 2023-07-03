d1b374b8a491de896ec7d81cbba86e8938a48fb4
Wenxiu Wang <59704238+wexw@users.noreply.github.com>
2023-07-03 14:38:23 +0200

    Add files via upload

--- 3d/Demo_JCC_3D.m
+++ 3d/Demo_JCC_3D.m
@@ -4,11 +4,9 @@ clear
 %close all
 
 path_mex_matlab1 = genpath('build/mex/');
-path_matlab = genpath('MATLAB');
 path_function=genpath('Functions_3D');
 
 addpath(path_mex_matlab1)
-addpath(path_matlab)
 addpath(path_function)
 
 
@@ -19,7 +17,7 @@ if USE_GPU_EXPDIST==0
     disp(['Please set the number of cores used here: Core_numer=']);
     Core_number=40;%%%You can set this number based on your hardware
 end
-
+CCD_pixelsize=130;
 cluster_mode=2;          %1: H,2:MDS
 Repeattimes=2;           %Number of independent JCC progress for a single NUP model
 sizeview=120; %radius of Field of interest
@@ -30,15 +28,21 @@ load('SimulatedNup107With65%DOL.mat')
 Particles=particles;
 
 Prapare_Input_particles
-
+M=size(V1,1);
+raster_spacing=30;       % expected resolution [nm]
 %Step1: Joint registration
 %Prepare parameters for JRMCP
-K = 18;
-%K=K_estimate(V2,meanlocs,20,0.5*raster_spacing, CCD_pixelsize);%Estimate number of Gaussian models
+disp(['Start to estimate the number of Gaussian components in GMM']);
+
+
+% K = 18;
+K=K_estimate(V2,meanlocs*2,30,raster_spacing, CCD_pixelsize);%Estimate number of Gaussian models
+disp(['The estimated number of Gaussian components in GMM is ' num2str(K)]);
+disp(['You can also set the K with prior knowledge']);
 Sa=sqrt(1000)./Cscale;% Size of initial Gaussian components
 nc=2; %number of clustersminClustSize=M/(nc+1);  %The minumum number of  the particle
 dimention=30;%round(N/6)*10; If classification doesn't work, try different dimention values
-
+minClustSize=M/(nc+1);
 Run_JRMPC3D
 
 
@@ -64,5 +68,6 @@ Draw3DAviProjection
 
 %%%You can also Draw the Plot by density plot
 %USE_GPU_GAUSSTRANSFORM = 0;
+%dip_initialise  %%diplib sould be loaded to use density plot function
 %Draw3DFigure%
 %%USE_GPU_GAUSSTRANSFORM is used here
