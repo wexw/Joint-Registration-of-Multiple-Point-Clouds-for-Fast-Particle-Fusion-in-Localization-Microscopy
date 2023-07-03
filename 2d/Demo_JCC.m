5656aa3524a47dbae78f1a2da49252119a64c473
Wenxiu Wang <59704238+wexw@users.noreply.github.com>
2023-07-03 13:57:05 +0200

    Add files via upload

--- 2d/Demo_JCC.m
+++ 2d/Demo_JCC.m
@@ -5,12 +5,11 @@ addpath(path_function)
 
 load('tud_120particles_dol80.mat')
 
-% % USE_GPU_GAUSSTRANSFORM = 0;
 USE_GPU_EXPDIST = 1;
 
 if USE_GPU_EXPDIST==0
-    disp(['Please set the number of cores used here: Core_numer=']);
-    Core_number=40;%%%You can set this number based on your hardware 
+    disp(['Please set the number of cores used here: Core_numer=...']);
+    Core_number=24;%%%You can set this number based on your hardware 
 end
 
 CCD_pixelsize=130;     %CCD_pixelsize for the data
@@ -19,11 +18,6 @@ raster_spacing=5;
 ParticleNumber=size(particles,2);%Number of input particles
 M=ParticleNumber;
 
-%estimate number of GMMcenters K for JRMPC
-disp(['Start to estimate the number of Gaussian components in GMM']);
-K =K_estimate(V1,meanlocs,20,0.8*raster_spacing,CCD_pixelsize); % Estimate the number of the GMM centers; We can also set the value.
-disp(['The estimated number of Gaussian components in GMM is ' num2str(K)]);
-disp(['You can also set the K with prior knowledge']);
 
 %Two parameters defined for the classification
 nc=2; %number of clusters
@@ -36,6 +30,11 @@ minClustSize=M/(nc+1);%The minumum number of the particle in a 'good' cluster
 %V1:particles' coordinates
 %mean locs: average number of localizations in the particles
 
+%estimate number of GMMcenters K for JRMPC
+disp(['Start to estimate the number of Gaussian components in GMM']);
+K =K_estimate(V1,meanlocs,20,0.8*raster_spacing,CCD_pixelsize); % Estimate the number of the GMM centers; We can also set the value.
+disp(['The estimated number of Gaussian components in GMM is ' num2str(K)]);
+disp(['You can also set the K with prior knowledge']);
 
 %Step1: Joint registration
 Run_JRMPC  %We can change Xin and JRMPC parameters inside
@@ -62,5 +61,3 @@ XX=cell2mat(TVPick')';%final localizations
 fig=renderprojection2D(XX(:,1),XX(:,2), [-80 80],[-80 80],1,1, 1);
 axis off
 axis equal
-%You can also Draw the Plot by density plot
-
