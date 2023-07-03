fe998a7e0d3a75946ca076c20790ccfff4c66b94
Wenxiu Wang <59704238+wexw@users.noreply.github.com>
2023-07-03 11:29:37 +0200

    Add files via upload

--- 3d/Demo_JCC_3D.m
+++ 3d/Demo_JCC_3D.m
@@ -13,12 +13,13 @@ addpath(path_function)
 
 
 % CPU/GPU settings (CPU = 0, GPU = 1)
-USE_GPU_GAUSSTRANSFORM = 0;
-USE_GPU_EXPDIST =0;
+
+USE_GPU_EXPDIST =1;
 if USE_GPU_EXPDIST==0
     disp(['Please set the number of cores used here: Core_numer=']);
     Core_number=40;%%%You can set this number based on your hardware
 end
+
 cluster_mode=2;          %1: H,2:MDS
 Repeattimes=2;           %Number of independent JCC progress for a single NUP model
 sizeview=120; %radius of Field of interest
@@ -35,9 +36,7 @@ Prapare_Input_particles
 K = 18;
 %K=K_estimate(V2,meanlocs,20,0.5*raster_spacing, CCD_pixelsize);%Estimate number of Gaussian models
 Sa=sqrt(1000)./Cscale;% Size of initial Gaussian components
-nc=2; %number of clusters
-cluster_mode=2; %2: MDS clustering, 1: Hierarchical agglomerative clustering approach as alternative to MDS
-minClustSize=M/(nc+1);  %The minumum number of  the particle
+nc=2; %number of clustersminClustSize=M/(nc+1);  %The minumum number of  the particle
 dimention=30;%round(N/6)*10; If classification doesn't work, try different dimention values
 
 Run_JRMPC3D
@@ -64,5 +63,6 @@ time_All=Time_JRMPC+time_Dissimilarity+Time_Connect+time_Classification; % Total
 Draw3DAviProjection
 
 %%%You can also Draw the Plot by density plot
-%Draw3DFigure
-
+%USE_GPU_GAUSSTRANSFORM = 0;
+%Draw3DFigure%
+%%USE_GPU_GAUSSTRANSFORM is used here
